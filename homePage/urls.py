bd00aac7ae8e3ae0bc392685a6c8719d996dd139
07akioni <07akioni2@gmail.com>
2016-12-13 04:36:33 -0800

    New logic, new ui.

--- homePage/urls.py
+++ homePage/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from homePage import views
 
 urlpatterns = [
-    url(r'^(?P<hash_str>[0-9]+)$', views.clipBoard),
+    url(r'^(?P<hash_str>[0-9]*)$', views.clipBoard),
 	url(r'^new/$', views.new_clipboard),
-	url(r'^get/$', views.get_clipboard),
-	url(r'^save/$', views.save_clipboard),
+	url(r'^get/(?P<hash_str>[0-9]+)$', views.get_clipboard),
+	url(r'^post/$', views.post_clipboard),
 ]
--- homePage/views.py
+++ homePage/views.py
@@ -1,4 +1,5 @@
 import random
+import json
 
 from django.utils import timezone
 from django.shortcuts import render
@@ -9,32 +10,85 @@ from homePage import models
 
 # Create your views here.
 def welcome(request) :
-	return render(request, 'home.html')
+	context = {}
+	try :
+		referer = request.META['HTTP_REFERER']
+		tmp = referer.split('/clipboard/')
+		if len(tmp) == 2 :
+			hash_str = tmp[1]
+			if hash_str.isdigit() :
+				context['hash_str'] = int(hash_str)
+			else :
+				tmp = tmp[1].split('?hash_str=')
+				if len(tmp) == 2 :
+					hash_str = tmp[1]
+					if hash_str.isdigit() :
+						context['hash_str'] = int(hash_str)
+		if context['hash_str'] is not None :
+			try :
+				models.Clipboard.objects.get(hash_str = context['hash_str'])
+			except models.Clipboard.DoesNotExist:
+				context['hash_str'] = None;
+		return render(request, 'home.html', context = context)
+	except KeyError:
+		return render(request, 'home.html', context = context)
 
 def new_clipboard(request) :
-	print('new_clipboard')
-	hash_str = str(random.randint(10000, 100000))
-	clipBoard = models.Clipboard(hash_str = hash_str, date_time = timezone.now())
-	clipBoard.save()
-	return HttpResponseRedirect('/clipboard/' + hash_str)
+	count = 20
+	while count > 0 :
+		hash_str = str(random.randint(10000, 100000))
+		try :
+			count -= 1
+			models.Clipboard.objects.get(hash_str = hash_str)
+		except models.Clipboard.DoesNotExist :
+			clipBoard = models.Clipboard(hash_str = hash_str, date_time = timezone.now())
+			clipBoard.save()
+			return HttpResponseRedirect('/clipboard/' + hash_str)
+	context = {}
+	context['error_mesg'] = '剪贴板太多 网站装不下了'
+	return render(request, 'notfindclipboard.html', context)
 
 def clipBoard(request, hash_str) :
 	context = {}
-	clipboard = models.Clipboard.objects.get(hash_str = hash_str)
-	context['clipboard'] = clipboard
-	# print(context)
-	# return HttpResponse('What the fuck?')
-	return render(request, 'clipboard.html', context = context)
+	if hash_str == '':
+		hash_str = request.GET['hash_str']
+	if hash_str == '':
+		context['error_mesg'] = '搜索不能为空'
+		return render(request, 'notfindclipboard.html', context = context)
+	try :
+		clipboard = models.Clipboard.objects.get(hash_str = hash_str)
+		context['clipboard'] = clipboard
+		return render(request, 'clipboard.html', context = context)
+	except models.Clipboard.DoesNotExist:
+		context['hash_str'] = hash_str
+		return render(request, 'notfindclipboard.html', context = context)
 
-def get_clipboard(request) :
+def get_clipboard(request, hash_str) :
 	context = {}
-	hash_str = request.GET['hash_str']
-	return HttpResponseRedirect('/clipboard/' + hash_str)
+	try :
+		clipboard = models.Clipboard.objects.get(hash_str = hash_str)
+		if clipboard.content == None :
+			clipboard.content = ''
+		context['content'] = clipboard.content
+		print(json.dumps(context))
+		return HttpResponse(json.dumps(context), content_type = 'text/json')
+	except models.Clipboard.DoesNotExist :
+		context['hash_str'] = hash_str
+		return render(request, 'notfindclipboard.html', context = context)
 
-def save_clipboard(request) :
-	hash_str = request.POST['id']
-	content = request.POST['content']
-	clipboard = models.Clipboard.objects.get(hash_str = hash_str)
-	clipboard.content = content
-	clipboard.save()
-	return HttpResponseRedirect('/clipboard/' + hash_str)
+def post_clipboard(request) :
+	if method == 'POST' :
+		try :
+			hash_str = request.POST['hash_str']
+			content = request.POST['content']
+			clipboard = models.Clipboard.objects.get(hash_str = hash_str)
+			clipboard.content = content
+			clipboard.save()
+			context = {'status' : 'success'}
+			return HttpResponse(json.dumps(context), content_type = 'text/json')
+		except:
+			context = {'status' : 'failed'}
+			return HttpResponse(json.dumps(context), content_type = 'text/json')
+	else :
+		context = {'status' : 'failed'}
+		return HttpResponse(json.dumps(context), content_type = 'text/json')
